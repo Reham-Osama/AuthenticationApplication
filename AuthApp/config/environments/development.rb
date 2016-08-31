d8f5500ba5497305795c0c48a3cc616cc96129c8
Maha Heikal <maha.heikal92@gmail.com>
2016-08-31 10:46:41 +0200

    Sending emails successfully in development env, just replace email and password in config/environments/development.rb

--- AuthApp/config/environments/development.rb
+++ AuthApp/config/environments/development.rb
@@ -40,6 +40,19 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # When using gmail, enable lesssecureapps in the account settings 
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'your_gmail',
+    password:       'your_password',
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 end
